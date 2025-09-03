7f82a0b21a029ce1dfb169118ae30798ad847c49
Antonio Marcos Sousa de Oliveira <antonioso@baraodemaua.br>
2025-09-03 17:56:06 -0300

    feature:adição da api

--- obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef83e2d0915d838757f2230318f20805e4a11c90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d4bfaf781e177262a9c54c0817597df7c2bd3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
